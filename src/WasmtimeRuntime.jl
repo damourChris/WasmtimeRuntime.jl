ab8ae1d109c0dd518316241289b8d0e2aa6e0e7d
Christopher <129872252+damourChris@users.noreply.github.com>
2025-06-24 21:58:19 +0200

    fix: add missing include and exports for Config struct

--- src/WasmtimeRuntime.jl
+++ src/WasmtimeRuntime.jl
@@ -6,11 +6,14 @@ using .LibWasmtime
 # Include all components in the correct order
 include("types.jl")
 include("errors.jl")
+include("config.jl")
 export WasmtimeObject, WasmtimeResource, WasmtimeValue, WasmtimeType
 export AbstractEngine, AbstractConfig
 export AbstractStore, AbstractModule, AbstractInstance
 export AbstractFunc, AbstractMemory, AbstractGlobal, AbstractTable
 # Error types
 export WasmtimeError
+export debug_info!,
+    optimization_level!, profiler!, consume_fuel!, epoch_interruption!, max_wasm_stack!
 
 end
