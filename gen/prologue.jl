393dd1546e6e8985b8110102e4f7ba086635f39c
Christopher <129872252+damourChris@users.noreply.github.com>
2025-06-24 15:28:14 +0200

    fix: add prologue for defining libwasmtime
    - see https://github.com/Pangoraw/Wasmtime.jl/blob/main/gen/prologue.jl

--- gen/prologue.jl
+++ gen/prologue.jl
@@ -8,3 +8,24 @@ This content appears before the auto-generated code in `LibWasmtime.jl`.
 """
 
 # Manual patches and additions to the generated bindings
+
+using Pkg.Artifacts
+using Pkg.BinaryPlatforms
+
+tripletnolibc(platform) = replace(triplet(platform), "-gnu" => "")
+wasmtime_folder_name(
+    platform,
+) = "wasmtime-v$release_version-$(tripletnolibc(platform))-c-api"
+
+function get_libwasmtime_location()
+    artifact_info = artifact_meta("libwasmtime", joinpath(@__DIR__, "..", "Artifacts.toml"))
+    artifact_info === nothing && return nothing
+
+    parent_path = artifact_path(Base.SHA1(artifact_info["git-tree-sha1"]))
+    child_folder = readdir(parent_path)[1]
+    return joinpath(parent_path, child_folder, "lib/libwasmtime")
+end
+
+const libwasmtime_env_key = "LIBWASMTIME_LOCATION"
+const libwasmtime =
+    haskey(ENV, libwasmtime_env_key) ? ENV[libwasmtime_env_key] : get_libwasmtime_location()
