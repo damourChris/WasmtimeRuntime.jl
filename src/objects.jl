41a3049af0bb1182c6d36e5a8f38a4b3d8ee20cf
Christopher <129872252+damourChris@users.noreply.github.com>
2025-06-26 12:18:33 +0200

    fix: update old names with new structs names

--- src/objects.jl
+++ src/objects.jl
@@ -3,9 +3,9 @@
 # Function implementation placeholder
 mutable struct Func <: AbstractFunc
     ptr::Ptr{LibWasmtime.wasmtime_func_t}
-    store::Store
+    store::WasmtimeStore
 
-    function Func(ptr::Ptr{LibWasmtime.wasmtime_func_t}, store::Store)
+    function Func(ptr::Ptr{LibWasmtime.wasmtime_func_t}, store::WasmtimeStore)
         func = new(ptr, store)
         # Functions are managed by the store, no finalizer needed
         return func
@@ -17,9 +17,9 @@ Base.isvalid(func::Func) = func.ptr != C_NULL
 # Memory implementation placeholder
 mutable struct Memory <: AbstractMemory
     ptr::Ptr{LibWasmtime.wasmtime_memory_t}
-    store::Store
+    store::WasmtimeStore
 
-    function Memory(ptr::Ptr{LibWasmtime.wasmtime_memory_t}, store::Store)
+    function Memory(ptr::Ptr{LibWasmtime.wasmtime_memory_t}, store::WasmtimeStore)
         memory = new(ptr, store)
         # Memories are managed by the store, no finalizer needed
         return memory
@@ -31,9 +31,9 @@ Base.isvalid(memory::Memory) = memory.ptr != C_NULL
 # Global implementation placeholder
 mutable struct Global <: AbstractGlobal
     ptr::Ptr{LibWasmtime.wasmtime_global_t}
-    store::Store
+    store::WasmtimeStore
 
-    function Global(ptr::Ptr{LibWasmtime.wasmtime_global_t}, store::Store)
+    function Global(ptr::Ptr{LibWasmtime.wasmtime_global_t}, store::WasmtimeStore)
         global_obj = new(ptr, store)
         # Globals are managed by the store, no finalizer needed
         return global_obj
@@ -45,9 +45,9 @@ Base.isvalid(global_obj::Global) = global_obj.ptr != C_NULL
 # Table implementation placeholder
 mutable struct Table <: AbstractTable
     ptr::Ptr{LibWasmtime.wasmtime_table_t}
-    store::Store
+    store::WasmtimeStore
 
-    function Table(ptr::Ptr{LibWasmtime.wasmtime_table_t}, store::Store)
+    function Table(ptr::Ptr{LibWasmtime.wasmtime_table_t}, store::WasmtimeStore)
         table = new(ptr, store)
         # Tables are managed by the store, no finalizer needed
         return table
@@ -57,7 +57,7 @@ end
 Base.isvalid(table::Table) = table.ptr != C_NULL
 
 # Export retrieval functions (basic implementation)
-function get_export(instance::Instance, name::String)
+function get_export(instance::WasmtimeInstance, name::String)
     isvalid(instance) || throw(WasmtimeError("Invalid instance"))
 
     export_ptr = Ref{LibWasmtime.wasmtime_extern_t}()
