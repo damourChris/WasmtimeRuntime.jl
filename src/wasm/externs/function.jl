38b10aeb2c02d905c2a0f61cbdfa68d29ee2c3af
Christopher <129872252+damourChris@users.noreply.github.com>
2025-07-05 18:14:34 +0200

    refactor: move default WasmFunc constructor out of struct definition

--- src/wasm/externs/function.jl
+++ src/wasm/externs/function.jl
@@ -34,91 +34,91 @@ results(func_type::WasmFuncType) = LibWasmtime.wasm_functype_results(func_type)
 mutable struct WasmFunc <: Function
     ptr::Ptr{LibWasmtime.wasm_func_t}
     func_type::WasmFuncType
+end
 
-    function WasmFunc(store::WasmStore, func::Function)
+function WasmFunc(store::WasmStore, func::Function)
 
-        # Create a function type based on the provided Julia function
-        # For now just use the first method signature
-        # TODO: support multiple signatures
-        func_method = first(Base.methods(func))
-        # Extract argument types (skip first parameter which is the function itself)
-        arg_types::Vector{DataType} = func_method.sig.parameters[2:end] |> collect
+    # Create a function type based on the provided Julia function
+    # For now just use the first method signature
+    # TODO: support multiple signatures
+    func_method = first(Base.methods(func))
+    # Extract argument types (skip first parameter which is the function itself)
+    arg_types::Vector{DataType} = func_method.sig.parameters[2:end] |> collect
 
-        # Get return type for this specific method signature
-        return_types = Base.return_types(func, Tuple{arg_types...})
-        rt = isempty(return_types) ? Any : return_types[1]
+    # Get return type for this specific method signature
+    return_types = Base.return_types(func, Tuple{arg_types...})
+    rt = isempty(return_types) ? Any : return_types[1]
 
-        function jl_side_host(
-            args::Ptr{wasm_val_vec_t},
-            results::Ptr{wasm_val_vec_t},
-        )::Ptr{wasm_trap_t}
-            try
-                # Convert wasm_val_vec_t to Julia types
-                wasm_args = unsafe_load(args)
+    function jl_side_host(
+        args::Ptr{wasm_val_vec_t},
+        results::Ptr{wasm_val_vec_t},
+    )::Ptr{wasm_trap_t}
+        try
+            # Convert wasm_val_vec_t to Julia types
+            wasm_args = unsafe_load(args)
 
-                converted_args = Vector{Any}(undef, wasm_args.size)
+            converted_args = Vector{Any}(undef, wasm_args.size)
 
-                for i = 1:wasm_args.size
-                    wasm_val = unsafe_load(wasm_args.data, i)
+            for i = 1:wasm_args.size
+                wasm_val = unsafe_load(wasm_args.data, i)
 
-                    # We transform the value to julia type based on args types
-                    if i <= length(arg_types)
-                        arg_type = arg_types[i]
-                        # @show wasm_val
-                        # @show "Trying tonverting to type: $(wasm_val.kind) -> $arg_type"
-                        converted_args[i] = convert(arg_type, wasm_val)
-                    else
-                        # If there are more wasm values than expected, we can ignore them
-                        continue
+                # We transform the value to julia type based on args types
+                if i <= length(arg_types)
+                    arg_type = arg_types[i]
+                    # @show wasm_val
+                    # @show "Trying tonverting to type: $(wasm_val.kind) -> $arg_type"
+                    converted_args[i] = convert(arg_type, wasm_val)
+                else
+                    # If there are more wasm values than expected, we can ignore them
+                    continue
 
-                    end
                 end
-
-                # @show converted_args
-
-                # Call the Julia function with converted arguments
-                res = func(converted_args...)
-
-                wasm_res = Ref(WasmValue(res))
-                data_ptr = unsafe_load(results).data
-                wasm_val_copy(data_ptr, wasm_res)
-
-                C_NULL
-            catch err
-                err_msg = string(err)
-                wasmtime_trap_new(err_msg, sizeof(err_msg))
             end
+
+            # @show converted_args
+
+            # Call the Julia function with converted arguments
+            res = func(converted_args...)
+
+            wasm_res = Ref(WasmValue(res))
+            data_ptr = unsafe_load(results).data
+            wasm_val_copy(data_ptr, wasm_res)
+
+            C_NULL
+        catch err
+            err_msg = string(err)
+            wasmtime_trap_new(err_msg, sizeof(err_msg))
         end
-
-        # Create a pointer to jl_side_host(args, results)
-        func_ptr = Base.@cfunction(
-            $jl_side_host,
-            Ptr{wasm_trap_t},
-            (Ptr{wasm_val_vec_t}, Ptr{wasm_val_vec_t})
-        )
-
-        functype = WasmFuncType(collect(arg_types), [rt])
-
-        host_func_ptr = LibWasmtime.wasm_func_new(store, functype, func_ptr)
-
-        @assert func_ptr != C_NULL "Failed to create WasmFunc"
-
-        # Add the function pointer to the store's externs
-        # This is necessary to ensure the function is not garbage collected
-        add_extern_func!(store, func_ptr)
-
-
-        wasmfunc = new(host_func_ptr, functype)
-
-        finalizer(wasmfunc) do f
-            if f.ptr != C_NULL
-                LibWasmtime.wasm_func_delete(f.ptr)
-                f.ptr = C_NULL
-            end
-        end
-
-        return wasmfunc
     end
+
+    # Create a pointer to jl_side_host(args, results)
+    func_ptr = Base.@cfunction(
+        $jl_side_host,
+        Ptr{wasm_trap_t},
+        (Ptr{wasm_val_vec_t}, Ptr{wasm_val_vec_t})
+    )
+
+    functype = WasmFuncType(collect(arg_types), [rt])
+
+    host_func_ptr = LibWasmtime.wasm_func_new(store, functype, func_ptr)
+
+    @assert func_ptr != C_NULL "Failed to create WasmFunc"
+
+    # Add the function pointer to the store's externs
+    # This is necessary to ensure the function is not garbage collected
+    add_extern_func!(store, func_ptr)
+
+
+    wasmfunc = WasmFunc(host_func_ptr, functype)
+
+    finalizer(wasmfunc) do f
+        if f.ptr != C_NULL
+            LibWasmtime.wasm_func_delete(f.ptr)
+            f.ptr = C_NULL
+        end
+    end
+
+    return wasmfunc
 end
 
 Base.unsafe_convert(::Type{Ptr{LibWasmtime.wasm_func_t}}, func::WasmFunc) = func.ptr
